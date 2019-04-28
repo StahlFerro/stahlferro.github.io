b3c454b94662fc9d7497ee9517b9a1d0e25f613d
StahlFerro <StahlFerro@users.noreply.github.com>
2019-04-29 03:57:40 +0700

    Now uses sass, FortressBot's page first prototype, columns on software pages now uses the multiline class instead of for-if liquid looping of alternating tags, navbar-burger now expands on mobile

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -27,14 +27,10 @@
 //   });
 
 $(document).ready(function() {
-    console.log("functioning")
-
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
-  
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-  
     });
   });
