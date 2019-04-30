b93c8e430ab65080032ce822ab993f0621aefded
StahlFerro <StahlFerro@users.noreply.github.com>
2019-04-30 17:02:29 +0700

    Moved bulma sass folders to static/sass/bulma, added threejs into static/js/threejs

--- static/js/3dtest.js
+++ static/js/3dtest.js
@@ -1,23 +1,36 @@
-import * as THREE from "./three.module.js"
+import * as THREE from "./threejs/three.module.js"
 
-var scene = new THREE.Scene();
+window.THREE = THREE || {}; 
+
+import { OrbitControls } from "./threejs/orbitcontrols.js"
+
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 1, 4);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x7289DA } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.3;
+controls.minPolarAngle = Math.PI / 2;
+controls.maxPolarAngle = Math.PI / 2;
+controls.update();
 
-camera.position.z = 5;
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh( geometry, material );
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+scene.add( cube );
 
 var animate = function () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
+    controls.update();
+    // cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
