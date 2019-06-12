86b6f5ec0a729b57bddd39b5f884ee9d0a82d8da
StahlFerro <StahlFerro@users.noreply.github.com>
2019-06-12 09:49:39 +0700

    Removed console.log from navbar.js:

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
-                console.log('logging...', el, $target);
             });
         });
     }
