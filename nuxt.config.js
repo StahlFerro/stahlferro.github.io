cfeaf56b72d616dde232896f63a08f9949f893fb
StahlFerro <StahlFerro@users.noreply.github.com>
2019-10-01 16:42:43 +0700

    Testing of meta tags configuration (OpenGraph especially)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,30 @@
+const ogconf = require('./config/opengraph.json');
+const meta = [
+  
+]
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'stahlferro.github.io',
+    title: "StahlFerro",
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt github page' }
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: x },
+      { hid: 'theme-color', name: 'theme-color', content: "#5595FF"},
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image'},
+      { hid: 'og:title', name: 'og:title', content: ogconf['og:title']}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/bulmamods.css'},
+      { rel: 'stylesheet', type: 'text/css', href: '/css/fontawesome-all.min.css'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Share+Tech|Share+Tech+Mono'},
     ]
   },
-  css: ['~/css/bulmamods.css'],
+  // css: ['~/css/bulmamods.css'],
   /*
   ** Customize the progress bar color
   */
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <template>
-<section class="hero is-medium is-white-e-iron-violet">
-    <div class="hero-body">
+  <div>
+    <section
+      class="hero is-fullheight-with-navbar is-black is-medium has-bg-center"
+      style="background-image: url('/static/img/Duality.png');">
+      <div class="hero-body"></div>
+    </section>
+    <section class="hero is-medium is-dark-2">
+      <div class="hero-body">
         <div class="container">
-            <div class="content">
-                <h1 class="title is-1">Creator | Developer | Video Compositor</h1>
-                <p>I create hybrid vfx/composited parody videos and develop various interesting softwares.</p>
-                <p>On this website, you will find my projects on both Videos and Softwares that I'm currently working on.
-                    The About page explains who I am and what I do around the cyberspace.
-                </p>
-            </div>
+          <div class="content">
+            <h1 class="title is-1">Creator | Developer | Video Compositor</h1>
+            <p>I create hybrid vfx/composited parody videos and develop various interesting softwares.</p>
+            <p>
+              On this website, you will find my projects on both Videos and Softwares that I'm currently working on.
+              The About page explains who I am and what I do around the cyberspace.
+            </p>
+          </div>
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+  </div>
 </template>
