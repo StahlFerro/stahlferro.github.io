ccad56d6afed11404ad36ccfb93dca62c1dc2264
StahlFerro <StahlFerro@users.noreply.github.com>
2019-10-03 01:32:38 +0700

    Added custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <section class="hero is-fullheight-with-navbar">
+            <div class="hero-body">
+                <div class="container">
+                <div class="content has-text-centered">
+                    <h1 class="title is-1"><span style="font-size: 80px;">
+                        404 Error!!!1</span></h1>
+                    <h1 class="title is-3"><span style="font-size: 50px;">
+                        Page not found m9</span></h1>
+                    <h1 class="title is-3">¯\_(ツ)_/¯</h1>
+                </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
