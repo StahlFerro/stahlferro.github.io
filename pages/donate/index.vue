d58e6fffe08adc328721f0075fe71b7aa80455f2
StahlFerro <StahlFerro@users.noreply.github.com>
2019-10-04 05:05:39 +0700

    Donation page prototype

--- pages/donate/index.vue
+++ pages/donate/index.vue
@@ -4,7 +4,47 @@
             <div class="hero-body">
                 <div class="container">
                     <div class="content has-text-centered">
-                        <h1 class="title is-1">donut</h1>
+                        <h1 class="title is-1">Donations</h1>
+                        <p>Hello there. I would like to highlight my current situation regarding to why I am opening a donation</p>
+                        <p>I am currently working as a software engineer. However, the pay is not enought to cover our financial stress, mainly for both me and my younger 
+                            sibling's university fees.
+                        </p>
+                        <p>Unfortunately, a lot of the available postings on my island offer roughly the same low wages, most even lower. A few even dare to 'trap' 
+                            desperate students on a 1 year contract that pays 1/3 of the minimum wage. Adding more to that, unemployment has risen drastically since late 2018, 
+                            and searching up till now, no well-paying software job postings are on sight.
+                        </p>
+                        <p>
+                            In one of my focused attempts to end our decade-long financial issues and overcome the nationwide economic disparity, I decided that it might be 
+                            viable to turn what I am the most passionate on, into someday, a proper job. Making VFX-animation videos and developing software that are free and 
+                            open source, in hopes to provide both entertainment  and technical solutions to everyone without charge. I know how enormously time-and-effort-consuming 
+                            it is to make or maintain just only one between those two and how much I don't care wasting all of my sleep time to put more work on both of them, 
+                            all the bills still have to be paid. There's not a lot of golden opportunities in my local environment. So, through this international window,
+                            I kindly ask, for your support.
+                        </p>
+                        <a class="button is-neon-gold is-large">
+                            <span class="icon">
+                                <i class="fab fa-patreon"></i>
+                            </span>
+                            <span>Patreon</span>
+                        </a>
+                        <a class="button is-neon-gold is-large">
+                            <span class="icon">
+                                <i class="fas fa-coffee"></i>
+                            </span>
+                            <span>Ko-fi</span>
+                        </a>
+                        <a class="button is-neon-gold is-large">
+                            <span class="icon">
+                                <i class="fab fa-patreon"></i>
+                            </span>
+                            <span>Liberapay</span>
+                        </a>
+                        <a class="button is-neon-gold is-large">
+                            <span class="icon">
+                                <i class="fab fa-paypal"></i>
+                            </span>
+                            <span>Direct Paypal</span>
+                        </a>
                     </div>
                 </div>
             </div>
