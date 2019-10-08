809b8fdc7f9e50a01d1efe127f644d773af8978e
StahlFerro <StahlFerro@users.noreply.github.com>
2019-10-09 03:08:48 +0700

    Updated robots.txt to include 404.html

--- deploy.ps1
+++ deploy.ps1
@@ -6,3 +6,6 @@ git add .;
 git commit -m "$msg";
 git push origin dev;
 npm run deploy;
+git checkout master;
+git pull;
+git checkout dev;
--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ git add .
 git commit -m "$msg"
 git push origin dev
 npm run deploy
+
+git checkout master
+git pull
+git checkout dev
