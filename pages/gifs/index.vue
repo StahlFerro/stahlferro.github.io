ee617d47b741891d30ff3566415322336d600dd6
StahlFerro <StahlFerro@users.noreply.github.com>
2019-10-11 02:37:49 +0700

    Menu and gif panels are now parented by columns instead of table cells

--- pages/gifs/index.vue
+++ pages/gifs/index.vue
@@ -1,10 +1,13 @@
 <template>
   <section class="hero has-background-dark-2 is-fullheight-with-navbar">
-    <div class="container">
-      <table class="gif-gallery-container" width="100%">
+    <div class="gallery-main-container">
+      <!-- <table class="gallery-table" width="100%">
         <tr>
-          <td width="10%">
-            <aside class="menu has-text-centered" style="margin: 0;">
+      <td width="10%" class="gallery-sticky-menu-column">-->
+      <div class="columns">
+        <div class="column is-2">
+          <div class="gallery-menu-container">
+            <aside class="menu" style="margin: 0;">
               <ul class="menu-list left-menu">
                 <li
                   v-for="(collection, index) in gifs"
@@ -18,41 +21,42 @@
                 </li>
               </ul>
             </aside>
-          </td>
-          <td width="90%">
-            <div class="container">
-              <div
-                class="columns is-multiline"
-                v-for="(collection, index) in gifs"
-                width="100%"
-                v-show="menuselection == collection.page_url"
-                v-bind:key="index"
-              >
-                <div class="column is-one-third" v-for="(gif, index) in collection.medias" v-bind:key="index">
-                  <div class="card is-neon-gold">
-                    <div class="card-image">
-                      <div class="video-container">
+          </div>
+        </div>
+        <div class="column is-10">
+          <div class="gallery-columns-container">
+            <div class="columns is-multiline" width="100%"
+              v-for="(collection, index) in gifs" v-show="menuselection == collection.page_url" v-bind:key="index">
+              <div class="column is-one-third"
+                v-for="(gif, index) in collection.medias" v-bind:key="index">
+                <div class="card is-neon-trident-cyan">
+                  <div class="card-image">
+                    <div class="video-container">
                       <video controls="controls" autoplay="autoplay" loop="loop">
                         <source
                           v-bind:src="`/gifstash/${collection.page_url}/${gif.fname_mp4}`"
                           type="video/mp4"
                         />
                       </video>
-                      </div>
                     </div>
-                    <div class="card-content">
-                      <div class="content">
-                        <p class="title is-4">{{ gif.name }}</p>
-                        <p class="content">{{ gif.description }}</p>
-                      </div>
+                  </div>
+                  <div class="card-content">
+                    <div class="content">
+                      <p class="title is-4">{{ gif.name }}</p>
+                      <p class="content">{{ gif.description }}</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </td>
+          </div>
+        </div>
+      </div>
+      <!-- </td> -->
+      <!-- <td width="90%" height="100%"> -->
+      <!-- </td>
         </tr>
-      </table>
+      </table>-->
     </div>
   </section>
 </template>
