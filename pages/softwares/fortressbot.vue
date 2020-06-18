cab0ab26d572c839db8c6130a5b7de327eb5a760
StahlFerro <StahlFerro@users.noreply.github.com>
2020-06-19 03:49:21 +0700

    Update EOL of FortressBot

--- pages/softwares/fortressbot.vue
+++ pages/softwares/fortressbot.vue
@@ -1,129 +1,17 @@
 <template>
   <div>
-    <section class="hero is-medium is-black">
+    <section class="hero is-fullheight-with-navbar is-black">
       <div class="hero-body">
         <div class="container has-text-centered">
           <img src="/logos/Fortress_200px.png" width="200px" height="200px" />
           <h1 class="title is-1">FortressBot</h1>
           <p class="subtitle is-4">The bane of server invite links and hoisters</p>
-          <p>
-            <a href="https://discordbots.org/bot/300611567874080769">
-              <img
-                src="https://discordbots.org/api/widget/servers/300611567874080769.svg?noavatar=true"
-                alt="FortressBot"
-              />
-            </a>
-          </p>
-          <br />
-          <p>
-            <a
-              href="https://discordapp.com/oauth2/authorize?client_id=300611567874080769&scope=bot&permissions=1560673535"
-              class="button is-neon-blurple is-large"
-            >
-              <span class="icon">
-                <i class="fab fa-discord"></i>
-              </span>
-              <span>Add to your server</span>
-            </a>
-          </p>
-          <br />
-          <div class="has-text-centered">
-            <div class="field is-grouped is-grouped-centered">
-              <p class="control">
-                <a v-bind:href="discord_server_url" class="button is-neon-trident-cyan is-medium">
-                  <span class="icon">
-                    <i class="fas fa-info-circle"></i>
-                  </span>
-                  <span>HQ/Support</span>
-                </a>
-              </p>
-              <p class="control">
-                <a href="https://fortressbot.readthedocs.io" class="button is-neon-grey is-medium">
-                  <span class="icon">
-                    <i class="fas fa-book"></i>
-                  </span>
-                  <span>Documentation</span>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="hero is-black">
-      <div class="hero-body">
-        <div class="container">
-          <div class="content">
-            <p class="title is-2 has-text-centered">
-              <span class="is-iron-red">Primarily</span> built for moderation
-            </p>
-            <ul>
-              <li class="is-iron-red">Administration</li>
-              <ul>
-                <li>User moderation (mute, temp mute, kick, ban, mass-kick, mass-ban, role management, etc.)</li>
-                <li>Server moderation (message mass-prune, pinning messages, export channel history, etc.)</li>
-              </ul>
-              <li class="is-iron-red">InviteNuke (The automatic invite link remover)</li>
-              <ul>
-                <li>Activate or deactivate the InviteNuke</li>
-                <li>Set invite exceptions</li>
-                <li>Set role exceptions</li>
-                <li>Set channel exceptions</li>
-                <li>Logging all InviteNuke actions</li>
-              </ul>
-              <li class="is-iron-red">Lockdown [UNDER CONSTRUCTION]</li>
-              <ul>
-                <li>Channel lock</li>
-                <li>Server lock</li>
-              </ul>
-              <li
-                class="is-iron-red"
-              >NameWash (Auto-dehoist and ascii-fy of usernames by assigning a cleaned version as their nicknames)</li>
-              <ul>
-                <li>Activate or deactivate the NameWash</li>
-                <li>Set different wash modes between ASCIIfy, dehoist or both</li>
-                <li>Set what characters are considered as hoisting</li>
-                <li>Logging all NameWash actions</li>
-              </ul>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="hero is-black">
-      <div class="hero-body">
-        <div class="container">
-          <div class="content">
-            <p class="title is-2 has-text-centered">
-              With various
-              <span class="is-iron-blue">secondary</span> utilities
-            </p>
-            <ul>
-              <li
-                class="is-iron-blue"
-              >Utility (a large collection of discord related utility commands)</li>
-              <ul>
-                <li>User, server, channel and role information lookup</li>
-                <li>Emote magnification</li>
-                <li>..and much more</li>]
-              </ul>
-              <li
-                class="is-iron-blue"
-              >Technical (a collection of commands for non-discord technical utilities)</li>
-              <ul>
-                <li>Character information lookup</li>
-                <li>String utilities</li>
-                <li>...and others</li>
-              </ul>
-              <li class="is-iron-blue">Entertainment (The ubiquitous fun commands)</li>
-              <ul>
-                <li>1v1</li>
-                <li>rate</li>
-                <li>roll</li>
-                <li>...and many more</li>
-              </ul>
-            </ul>
-          </div>
+          <br/>
+          <h1 class="title is-1">Rest In Peace</h1>
+          <h2 class="title is-2">2017-04-09 - 2020-06-19</h2>
+          <p>Thank you very much for those that has used FortressBot during it's time of service. I have no more spare time left to properly maintain FortressBot and fix its bugs.
+            All that is left is a bot that has been terribly underperforming for months. It is not fit to serve realiable automoderative actions anymore.</p>
+          <p>I declare its End of Life effective on date 2020/06/19. Goodbye FortressBot. You have served well.</p>
         </div>
       </div>
     </section>
