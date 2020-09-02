97f50b6a43b5cf2054eb8fc7958049f392c8ef9d
StahlFerro <StahlFerro@users.noreply.github.com>
2020-09-02 16:15:29 +0700

    Master now contains the dev code from dev branch, and the gh-pages will be the one served by Github Pages; Updated deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -4,8 +4,8 @@ yarn genpage;
 git add .;
 
 git commit -m "$msg";
-git push origin dev;
+git push origin master;
 yarn push2ghpages;
 git checkout gh-pages;
 git reset --hard origin/gh-pages;
-git checkout dev;
+git checkout master;
